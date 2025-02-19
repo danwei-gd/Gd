e2e60bddccae7c324920df19f3696fb01f0988fe
Dan Wei <dan.wei@greendotcorp.com>
2025-02-19 17:31:58 +0800

    profile view

--- Dan/Controllers/ProfileController.cs
+++ Dan/Controllers/ProfileController.cs
@@ -1,4 +1,6 @@
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using System.Threading.Tasks;
 
 namespace Dan;
@@ -7,6 +9,14 @@ public class ProfileController : Controller
 {
 	public async Task<IActionResult> Index()
 	{
-		return View();
+		using var conn = new SqlConnection("Server=dv-gss-eus-gft-001-sql.database.windows.net; Database=GlobalFundTransfer;  Authentication=Active Directory Default; Encrypt=True; Column Encryption Setting=Enabled;Connect Timeout=30;");
+		var model = await conn.QueryAsync(@"
+				select 
+				* 
+				from GlobalFundTransferProfile(nolock) 
+				order by GlobalFundTransferProfileKey 
+				offset 10 rows fetch next 10 rows only
+				", commandTimeout: 3600);
+		return View(model);
 	}
 }
